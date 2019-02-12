d628e8dd0371580816666b8d009494c8692d764c
bbursey <bbursey@emoneyadvisor.com>
2019-02-12 09:53:07 -0500

    Extract months into its own file

--- Countdown/TimeSpanExtensions.cs
+++ Countdown/TimeSpanExtensions.cs
@@ -6,22 +6,7 @@ namespace Countdown
 {
     public static class TimeSpanExtensions
     {
-        private enum Months
-        {
-            January = 31,
-            February = 28,
-            March = 31,
-            April = 30,
-            May = 31,
-            June = 30,
-            July = 31,
-            August = 31,
-            September = 30,
-            October = 31,
-            November = 30,
-            December = 31
-        };
-
+       
         public static AnnualTime TimeLeft(this TimeSpan timeLeft, DateTime startDate)
         {
             var daysLeft = timeLeft.Days;
